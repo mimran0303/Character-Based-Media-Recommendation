4396404f9ae825b2271cafa318f5b220a4c5a06e
Sana Akhtar <sanaakhtar@MacBookAir.attlocal.net>
2024-12-23 15:50:46 -0600

    sana page

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # Character Recommendation System
-Based on the characters you like or your MBTI and Enneagram, you will be recommended other characters and the movies they're in!
-
-# Pulling updated version
-step 1: git fetch
-step 2: git pull
+Based on the characters you like, you will be recommended other characters and the movies they're in!
---
+++ my-app/src/output.js
@@ -0,0 +1,35 @@
+import './output.css';
+
+function output() {
+    return (
+      <div className="output">
+        <header className="output-header">
+          <div className="Header-title">
+            <h1>✨ List of Characters ✨</h1>
+          </div>
+        </header>
+        <div className="Paragraph-Description">
+          <p>Top 10 Movies/Characters:</p>
+          <ol>
+            <li>Character 1</li>
+            <li>Character 2</li>
+            <li>Character 3</li>
+            <li>Character 4</li>
+            <li>Character 5</li>
+            <li>Character 6</li>
+            <li>Character 7</li>
+            <li>Character 8</li>
+            <li>Character 9</li>
+            <li>Character 10</li>
+          </ol>
+        </div>
+        {/* Restart Button */}
+        <button type="button" className="button">
+          Restart!
+        </button>
+      </div>
+    );
+  }
+  
+
+export default output;
